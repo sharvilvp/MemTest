779a089f5ffbab18a7e66a46611a7293be5e1c34
sharvilvp <sharvilvp@gmail.com>
2019-04-14 02:15:40 -0700

    Adding Files

--- memory_defines.h
+++ memory_defines.h
@@ -1,2 +1,30 @@
+#ifndef _MEMORY_DEFINES_H_
+#define _MEMORY_DEFINES_H_
 #define DRF_DELAY 50
 #define MEM_SIZE 10
+#define STUCK_AT_FAULT_PROBABILITY 1
+#define TRANSITION_FAULT_PROBABILITY 5
+#define INVERSION_FAULT_PROBABILITY 1
+#define FAULT_PROBABILITY 20
+#include<vector>
+#include<tuple>
+typedef enum {
+    low2high,
+    high2low,
+    no_tran
+} transition_t;
+
+typedef enum {
+    R,
+    W,
+    NOP
+} op_t;
+typedef enum {
+    INC,
+    DEC,
+    ANY
+} order_t;   
+
+typedef std::tuple<op_t,uint32_t> march_operation_t;
+typedef std::vector<std::tuple<op_t,uint32_t>> march_element_t;
+#endif
